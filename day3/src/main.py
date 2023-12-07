235efa87a9299a43083e76f9a980f6030f8f7d03
NxtPerfect <alakaxan@protonmail.com>
2023-12-07 20:44:03 +0100

    Day 3 part 2

--- day3/src/main.py
+++ day3/src/main.py
@@ -1,19 +1,19 @@
 from collections import defaultdict
 
-# We got the symbols
-# Now if we can somehow save the numbers into list, and their starting position, we can find
-# which number touches the symbol by adding length to position, and if it's same or more than
-# the symbol, we're good, if starting position -1 is symbol, we're also good
 
-# Go two lines at a time, if we find symbol, then go back one and forward one, if there's a number, add it
-# 467..114..
-# ...*......
+# ......755.
+# ...$.*....
+# .664.598..
+# Gear ratio is calculated by multiplying two numbers that both touch the same symbol
+# Then i need to sum them all up
 
 
 def run(file='./input.txt'):
     total = 0
-    symbols_in_line = list()
-    symbols, digits = defaultdict(list), defaultdict(list)
+    gears = 0
+    symbols_in_line, stars_in_line = list(), list()
+    symbols, stars, digits = defaultdict(
+        list), defaultdict(list), defaultdict(list)
     f = open(file)
     lines = f.readlines()
     for line in lines:
@@ -29,28 +29,28 @@ def run(file='./input.txt'):
                 digits[lines.index(line)].append((start, temp))
                 temp = ""
                 start = -1
-            # print(digits.values())
 
+    # Part one
     for line in lines:
         for char in range(len(line)):
             if line[char].isdigit() or line[char] == '.' or line[char] == '\n' or line[char] == ' ':
                 continue
-            # I Need to find the digits, so that i get starting point of them
-            # and the whole number
+            if line[char] == '*':
+                stars_in_line.append(char)
             symbols_in_line.append(char)
-            # print(symbols_in_line)
+        stars[lines.index(line)] = stars_in_line
+        stars_in_line = list()
         symbols[lines.index(line)] = symbols_in_line
-        # print(symbols.values())
         symbols_in_line = list()
 
     print(symbols.values())
     for row, column in symbols.items():
-        print(column)
-
+        # print(column)
         for i, x in enumerate(column):
+            # print(x)
             for t in [-1, 0, 1]:
-                print(
-                    f'Iteration {i+t} for column {row}, with numbers {digits[row+t]}')
+                # print(
+                #     f'Iteration {i+t} for column {row}, with numbers {digits[row+t]}')
                 a = row + t
                 if a < 0 or a >= len(digits):
                     continue
@@ -59,15 +59,49 @@ def run(file='./input.txt'):
                     if y[0] == 0:
                         start = y[0]
                     if x not in range(start, y[0] + len(y[1])+1):
-                        print("not found")
+                        # print("not found")
                         continue
-                    print(f'{y[1]} found for col {row}')
+                    # print(f'{y[1]} found for col {row}')
                     total += int(y[1])
 
-    print(total)
+    # Part two
+    for row, column in stars.items():
+        print(column)
+        for x in column:
+            print(x)
+            gearratio = 0
+            one, two = False, False
+            for t in [-1, 0, 1]:
+                a = row + t
+                if a < 0 or a >= len(digits):
+                    continue
+                for y in digits[a]:
+                    start = y[0] - 1
+                    if y[0] == 0:
+                        start = y[0]
+                    if x not in range(start, y[0] + len(y[1])+1):
+                        continue
+                    print(f'Found {y[1]}')
+                    print(f'Ratio: {gearratio}')
+                    if not one:
+                        gearratio = int(y[1])
+                        one = True
+                        continue
+                    if one and two:
+                        gearratio = 0
+                        continue
+                    if one:
+                        gearratio *= int(y[1])
+                        two = True
+            print(f'Gears col: {gears}')
+            if not two:
+                gearratio = 0
+            gears += gearratio
+
+    print("Total: " + str(total))
+    print("Gears: " + str(gears))
     return total
 
 
 if __name__ == "__main__":
-    print("Did i get correct reults? " + str(run() == 4361))
-    print("Should be 4361")
+    run()
