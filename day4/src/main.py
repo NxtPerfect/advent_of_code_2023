ca818f8a4badbb26420834f45cb61cde4aa3443a
NxtPerfect <alakaxan@protonmail.com>
2023-12-08 12:56:18 +0100

    Day 4 part 2

--- day4/src/main.py
+++ day4/src/main.py
@@ -1,38 +1,31 @@
+from collections import defaultdict
 import time
 
-# Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-# For each number that we matched, we double points, first match is worth one point
-# Here we matched 86, 17, 48 and 53, so it's worht 8 points (1, 2, 4, 8)
-# Left is winning numbers, right is our numbers
 
-
-def run(file="./input.txt"):
-    total = 0
+def run(file="input.txt"):
+    # Part two
+    count = defaultdict(initDict)
     with open(file) as f:
-        for line in f:
-            lineTotal = 1/2
-            winningNum, ourNum = line.split("|")
-            _, winningNum = winningNum.split(":")
-            ourNum = " ".join(ourNum.split())
-            winningNum = " ".join(winningNum.split())
-            ourNum = ourNum.split(" ")
-            winningNum = winningNum.split(" ")
+        for idx, line in enumerate(f):
+            game, numb = line.strip().split(":")
+            winner, sample = numb.strip().split("|")
+            sample = sample.strip().split()
+            winner = winner.strip().split()
 
-            for x in ourNum:
-                if x in winningNum:
-                    lineTotal *= 2
-                #     print(f'Found {x} for {lineTotal} points')
-                # print(x)
-            total += int(lineTotal)
-    #         print(winningNum)
-    #         print(ourNum)
-    # print(total)
-    return total
+            numb_intersections = len(set(winner).intersection(set(sample)))
+
+            for update in range(numb_intersections):
+                count[idx+1+update] += count[idx]
+
+    print(sum(count.values()))
+
+
+def initDict():
+    return 1
 
 
 if __name__ == "__main__":
     start = time.perf_counter_ns()
-    total = run()
-    # print("Did we get correct number? " + str(total == 13))
+    run()
     end = time.perf_counter_ns()
-    print("Runtime of " + str(end-start) + "ns")
+    print("Runtime of " + str((end-start)/1000000) + "ms")
