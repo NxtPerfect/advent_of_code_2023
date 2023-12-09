13987e4baa04fc18d40b9d7c7ab827138ff518c3
NxtPerfect <alakaxan@protonmail.com>
2023-12-09 20:52:32 +0100

    Removed input files

--- day5/src/main.py
+++ day5/src/main.py
@@ -12,50 +12,65 @@ import time
 # Another is temporary, holding values between conversions
 
 
-def run(file="input.txt"):
-    seeds = [1] * 20
-    tempSeeds = seeds.copy()
+def run(file="test.txt"):
+    seeds = list()
+    # tempSeeds = seeds.copy()
+    new_ranges = list()
     conversion = [1] * 3
     with open(file) as f:
         for idx, line in enumerate(f):
             # Copy value of seeds if we run out of values to compare to
             if line == "\n" or line.find("map:") != -1:
-                tempSeeds = seeds.copy()
+                seeds.append(new_ranges)
+                print("Append seeds")
+                # tempSeeds = seeds.copy()
                 continue
             # Initial run
             if idx == 0:
-                _, seeds = line.strip().split(":")
-                seeds = map(int, seeds.strip().split(" "))
-                seeds = list(seeds)
-                tempSeeds = seeds.copy()
+                _, temp = line.strip().split(":")
+                # take numbers as ranges and add to seeds
+                _seeds = temp.strip().split(" ")
+                _seeds = list(map(int, _seeds))
+                for i in range(0, len(_seeds)-1, 2):
+                    seeds.append(range(_seeds[i],
+                                       _seeds[i] + _seeds[i+1]))
+                print(seeds)
+                # tempSeeds = seeds.copy()
                 continue
             # If tempSeeds was emptied, because we changed every value in list
-            if len(tempSeeds) == 0:
-                tempSeeds = seeds.copy()
+            # if len(tempSeeds) == 0:
+            #     tempSeeds = seeds.copy()
             conversion = line.strip().split(" ")
+            conversion = list(map(int, conversion))
+            change_range = range(conversion[1],
+                                 conversion[1] + conversion[2])
+            product_range = range(conversion[0],
+                                  conversion[0] + conversion[1])
             print("Line:", conversion)
-            prodStart, sourceStart, length = map(int, conversion)
+            # prodStart, sourceStart, length = map(int, conversion)
             # Iterate over seeds to change their values, if they're in tempSeeds, that means they weren't changed
             for idx, seed in enumerate(seeds):
-                if len(tempSeeds) == 0:
-                    break
-                if seed not in tempSeeds:
+                intersection_start = max(seed.start, change_range.start)
+                intersection_stop = max(seed.stop, change_range.stop)
+                if intersection_start > intersection_stop:
                     continue
-                if seed not in range(sourceStart, sourceStart+length):
-                    continue
-                seeds[idx] = prodStart + abs(seed - sourceStart)
-                tempSeeds.pop(tempSeeds.index(seed))
-            # seeds = [prodStart + abs(seed - sourceStart) if seed in range(
-            #     sourceStart, sourceStart + length) else seed for seed in seeds]
+                new_ranges.append(
+                    range(product_range.start, product_range.stop))
+                print("New ranges: ", new_ranges)
+                seeds[idx] = range(seed.start, intersection_start)
+                print("Creating tuple?",
+                      seeds[idx+1], type(seeds[idx+1]), seed.start, intersection_start)
             print(seeds)
-            print(tempSeeds)
-        print(min(seeds))
-        return min(seeds)
+            print(new_ranges)
+            # print(tempSeeds)
+        # print(min(seeds))
+        # return min(seeds)
 
 
 if __name__ == "__main__":
     start = time.perf_counter_ns()
     minimum = run()
-    print("Is it correct?", minimum == 35)
+    # print("Is it correct?", minimum == 35)
+    print("Is it correct?", minimum == 46)
     end = time.perf_counter_ns()
     print("Runtime of " + str((end-start)/1000000) + "ms")
