1089c233c26ab34d8fc1a83f0f84a3e92dc27c3a
NxtPerfect <alakaxan@protonmail.com>
2023-12-11 12:54:46 +0100

    Day 6 part 2

--- day6/src/main.py
+++ day6/src/main.py
@@ -1,49 +1,34 @@
-from collections import defaultdict
 import time
-import math
-
-# That will ensure our win, even if it's by a fraction
-# Time is in milliseconds, distance in millimiters
-# For each unit of time we spend holding button, our speed increases by 1 time/distance
-# Find all the combinations that still beat the record
 
 
 def run(file="input.txt"):
-    race = defaultdict(int)
-    ways = list()
+    ways = 0
 
     with open(file) as f:
         data = f.readlines()
 
-    tempTimes = data[0].strip().split(" ")
-    # tempTimes = list(filter(lambda x: x != '' or x != 'Time:', tempTimes))
-    tTimes = [x for x in tempTimes if x != '' and x != 'Time:']
-    tempTimes = map(int, tTimes)
-    tempDistance = data[1].strip().split(" ")
-    tDist = [x for x in tempDistance if x != '' and x != 'Distance:']
-    tempDistance = map(int, tDist)
+    tempTimes = int("".join(data[0].strip().split(":")[1].split()))
+    tempDistance = int("".join(data[1].strip().split(":")[1].split()))
 
-    race = {key: value for key, value in zip(tempTimes, tempDistance)}
+    # 0,1,2,...x,...key-x
+    x = int(tempTimes/tempDistance * tempDistance/4.9)
+    while True:
+        # for x in range(1, key-1):
+        if x * (tempTimes - x) <= tempDistance:
+            x += 1
+            continue
+        diff = x
+        ways = (tempTimes - 2*diff + 1)
+        break
 
-    for key, value in race.items():
-        curWays = list()
-        for x in range(1, key-1):
-            if x * (key - x) <= value:
-                continue
-            curWays.append(x)
-        ways.append(len(curWays))
-
-    print(tempTimes, tempDistance)
-    print(race)
-    print(ways)
-    print(math.prod(ways))
-    return math.prod(ways)
+    # print(ways)
+    return ways
 
 
 if __name__ == "__main__":
     start = time.perf_counter_ns()
     minimum = run()
-    print("Is it correct?", minimum == 288)
+    # print("Is it correct?", minimum == 288)
     # print("Is it correct?", minimum == 71503)
     end = time.perf_counter_ns()
     print("Runtime of " + str((end-start)/1000000) + "ms")
