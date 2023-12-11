4982ffc778c652375e82ae9e1d569d1d650ea826
NxtPerfect <alakaxan@protonmail.com>
2023-12-11 13:05:29 +0100

    Day 6 part 2

--- day6/src/main.py
+++ day6/src/main.py
@@ -10,10 +10,9 @@ def run(file="input.txt"):
     tempTimes = int("".join(data[0].strip().split(":")[1].split()))
     tempDistance = int("".join(data[1].strip().split(":")[1].split()))
 
-    # 0,1,2,...x,...key-x
+    # I don't know exact function, but this one is pretty close
     x = int(tempTimes/tempDistance * tempDistance/4.9)
     while True:
-        # for x in range(1, key-1):
         if x * (tempTimes - x) <= tempDistance:
             x += 1
             continue
@@ -28,7 +27,5 @@ def run(file="input.txt"):
 if __name__ == "__main__":
     start = time.perf_counter_ns()
     minimum = run()
-    # print("Is it correct?", minimum == 288)
-    # print("Is it correct?", minimum == 71503)
     end = time.perf_counter_ns()
     print("Runtime of " + str((end-start)/1000000) + "ms")
